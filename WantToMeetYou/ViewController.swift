70770fcc00df3c9bcc44005207f42315b1bd971d
Alex <smorodov@vigroup.ru>
2023-06-25 16:41:30 +0500

    initiate project

---
+++ WantToMeetYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantToMeetYou
+//
+//  Created by Александр Смородов on 25.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
